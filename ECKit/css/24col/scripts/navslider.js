7b2fba6efac82908b542b8bf1a0d3196089a777d
ECWebServices <ecwebservices@gmail.com>
2017-10-09 19:11:18 +0000

    V3 finished

---
+++ ECKit/css/24col/scripts/navslider.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px"; //Default: 250px. Edit to your liking.
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
--- ECKit/scripts/navslider.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px"; //Default: 250px. Edit to your liking.
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
